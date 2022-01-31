a684823542f6d78dfd57aaa00da319b3a2ab726b
cskwrd <cskwrd@users.noreply.github.com>
2022-01-31 13:11:12 -0600

    Add project files.

---
+++ src/Wyrm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wyrm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
