32e1cdacfcad7e60fd46dec55bfb9d17ea2386a4
cskwrd <cskwrd@users.noreply.github.com>
2022-02-23 20:08:58 -0600

    Add `hyper` to make HTTP requests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use hyper::Client;
+use hyper_tls::HttpsConnector;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -7,8 +9,20 @@ struct Args {
     book_url: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let args = Args::parse();
+    
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
 
-    println!("URL: '{}'", args.book_url);
+    // Parse an `http::Uri`...
+    let uri = args.book_url.parse()?;
+
+    // Await the response...
+    let resp = client.get(uri).await?;
+
+    println!("Response status: {}", resp.status());
+
+    Ok(())
 }
