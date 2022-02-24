b8c6ebd019a6f417604f7e4d9f339d0a62d2073e
cskwrd <cskwrd@users.noreply.github.com>
2022-02-23 20:06:08 -0600

    Add `clap` to parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The universal resource locator of book
+    book_url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("URL: '{}'", args.book_url);
 }
