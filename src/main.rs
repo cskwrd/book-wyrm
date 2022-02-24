30199cd14dbd1b5dbe24a1968b0925354a3e005c
cskwrd <cskwrd@users.noreply.github.com>
2022-02-23 20:12:10 -0600

    Write HTML to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use hyper::Client;
 use hyper_tls::HttpsConnector;
+use hyper::body::HttpBody as _;
+use tokio::io::{stdout, AsyncWriteExt as _};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -20,9 +22,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let uri = args.book_url.parse()?;
 
     // Await the response...
-    let resp = client.get(uri).await?;
+    let mut resp = client.get(uri).await?;
 
-    println!("Response status: {}", resp.status());
+    // Write HTML asynchronously to stdout...
+    while let Some(chunk) = resp.body_mut().data().await {
+        stdout().write_all(&chunk?).await?;
+    }
 
     Ok(())
 }
